78fab63d20b58ad1e4b0207a5fdbff6206d5eca8
sherlinz0 <sherly_cs_sh@163.com>
2022-03-08 20:51:18 +0800

    Add: AppHeader 完成 切换路由时样式改变

--- src/components/app-header/index.jsx
+++ src/components/app-header/index.jsx
@@ -1,9 +1,46 @@
-import React, { memo } from 'react'
+import React, { memo } from "react";
+
+import { headerLinks } from "@/common/local-date";
+
+import { NavLink } from "react-router-dom";
+
+import { HeaderWrapper, HeaderLeft, HeaderRight } from "./style";
 
 const AppHeader = memo(() => {
+  const renderNavs = (nav, index) => (
+    <li key={nav.key}>
+      {index < 3 ? (
+        <>
+          <NavLink to={nav.link} className="nav">
+            {nav.title}
+            <i className="subscript-icon sprite_01" />
+          </NavLink>
+        </>
+      ) : (
+        <>
+          <a href={nav.link} className="nav">
+            {nav.title}
+          </a>
+        </>
+      )}
+    </li>
+  );
+
   return (
-    <div>AppHeader</div>
-  )
-})
+    <HeaderWrapper>
+      {/* wrap-v1 已经在全局样式中定义 */}
+      <span className="content wrap-v1">
+        <HeaderLeft>
+          <a href="#/" className="logo sprite_01">网易云音乐</a>
+          <ul className="navs">{headerLinks.map(renderNavs)}</ul>
+          <NavLink to="/mine"></NavLink>
+          <NavLink to="/friend"></NavLink>
+        </HeaderLeft>
+        <HeaderRight></HeaderRight>
+      </span>
+      <div className="divider"></div>
+    </HeaderWrapper>
+  );
+});
 
 export default AppHeader;
--- src/components/app-header/style.js
+++ src/components/app-header/style.js
@@ -3,14 +3,80 @@ import styled from 'styled-components';
 
 export const HeaderWrapper = styled.div`
 	height: 75px;
+	color: #fff;
 	background-color: #242424;
 
 	.content {
 		height: 70px;
+		background-color: #242424;
+
+		display: flex;
+		justify-content: space-between;
 	}
 
 	.divider {
 		height: 5px;
 		background-color: #C20C0C;
 	}
+`;
+
+export const HeaderLeft = styled.div`
+	display: flex;
+
+	.logo {
+		display: block;
+		width: 176px;
+		height: 69px;
+		background-position: 0 0;
+		text-indent: -9999px;
+	}
+
+	.navs {
+		display: flex;
+		line-height: 70px;
+		font-size: 14px;
+
+		.nav {
+			display: inline-block;
+			position: relative;
+			padding: 0 19px;
+			height: 70px;
+			text-decoration: none;
+			color: #ccc;
+
+			:hover, &.active {
+				background-color: #000;
+				color: #fff;	
+			}
+
+			&.active .subscript-icon {
+				display: block;
+				position: absolute;
+				display: inline-block;
+				width: 12px;
+				height: 7px;
+				bottom: -1px;
+				left: 50%;
+				transform: translate(-50%, 0);
+				background-position: -226px 0;
+			}
+		}
+
+		li:last-child .nav{
+			::after {
+				position: absolute;
+				content: "";
+				width: 28px;
+				height: 19px;
+				background-image: url(${require("@/assets/img/sprite_01.png")});
+				background-position: -190px 0;
+				top: 20px;
+				right: -15px;
+			}
+		}
+	}
+`;
+
+export const HeaderRight = styled.div`
+
 `;
