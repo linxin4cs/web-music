846bd072c3950677fea27ba0bb19ab708a864e60
sherlinz0 <sherly_cs_sh@163.com>
2022-03-09 17:48:03 +0800

    Updata: 更新 local-data, 为每个子项添加 key 值

--- src/common/local-data.js
+++ src/common/local-data.js
@@ -1,98 +1,113 @@
 export const headerLinks = [
   {
     title: "发现音乐",
-    link: "/",
-    key: 'discover'
+    link: "/discover",
+    key: "discover",
   },
   {
     title: "我的音乐",
     link: "/mine",
-    key: 'mine',
+    key: "mine",
   },
   {
     title: "朋友",
     link: "/friend",
-    key: 'friend',
+    key: "friend",
   },
   {
     title: "商城",
     link: "https://music.163.com/store/product",
-    key: 'product',
+    key: "product",
   },
   {
     title: "音乐人",
     link: "https://music.163.com/nmusician/web/index#/",
-    key: 'nmusician',
+    key: "nmusician",
   },
   {
     title: "下载客户端",
     link: "https://music.163.com/#/download",
-    key: 'download',
-  }
-]
+    key: "download",
+  },
+];
 
 export const footerLinks = [
   {
     title: "服务条款",
-    link: "https://st.music.163.com/official-terms/service"
+    link: "https://st.music.163.com/official-terms/service",
+    key: "service",
   },
   {
     title: "隐私政策",
-    link: "https://st.music.163.com/official-terms/privacy"
+    link: "https://st.music.163.com/official-terms/privacy",
+    key: "privacy",
   },
   {
     title: "儿童隐私政策",
-    link: "https://st.music.163.com/official-terms/children"
+    link: "https://st.music.163.com/official-terms/children",
+    key: "children",
   },
   {
     title: "版权投诉指引",
-    link: "https://music.163.com/st/staticdeal/complaints.html"
+    link: "https://music.163.com/st/staticdeal/complaints.html",
+    key: "complaints",
   },
   {
     title: "意见反馈",
-    link: "#"
-  }
-]
+    link: "#",
+    key: "opinion",
+  },
+];
 
 export const footerImages = [
   {
-    link: "https://music.163.com/st/userbasic#/auth"
+    link: "https://music.163.com/st/userbasic#/auth",
+    key: "auth",
   },
   {
-    link: "https://music.163.com/recruit"
+    link: "https://music.163.com/recruit",
+    key: "recruit",
   },
   {
-    link: "https://music.163.com/web/reward"
+    link: "https://music.163.com/web/reward",
+    key: "reward",
   },
   {
-    link: "https://music.163.com/uservideo#/plan"
-  }
-]
+    link: "https://music.163.com/uservideo#/plan",
+    key: "plan",
+  },
+];
 
 // discover中的数据
 export const dicoverMenu = [
   {
     title: "推荐",
-    link: "/discover/recommend"
+    link: "/discover/recommend",
+    key: "recommend",
   },
   {
     title: "排行榜",
-    link: "/discover/ranking"
+    link: "/discover/ranking",
+    key: "ranking",
   },
   {
     title: "歌单",
-    link: "/discover/songs"
+    link: "/discover/songs",
+    key: "songs",
   },
   {
     title: "主播电台",
-    link: "/discover/djradio"
+    link: "/discover/djradio",
+    key: "djradio",
   },
   {
     title: "歌手",
-    link: "/discover/artist"
+    link: "/discover/artist",
+    key: "artist",
   },
   {
     title: "新碟上架",
-    link: "/discover/album"
+    link: "/discover/album",
+    key: "album",
   },
-]
+];
