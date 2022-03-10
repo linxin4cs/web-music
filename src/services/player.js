9e7d142dd737a54a53ee6b6c20a6336244ef0de4
sherlinz0 <sherly_cs_sh@163.com>
2022-03-10 18:30:04 +0800

    Add: 添加 播放器 相关请求的数据实体化处理

--- src/services/player.js
+++ src/services/player.js
@@ -1,19 +1,32 @@
-import request from './request';
+import request from "./request";
 
-export function getSongDetail(ids) {
+import { Song, Singer } from "@/entity/player";
+
+export function getSong(ids) {
   return request({
     url: "/song/detail",
     params: {
-      ids
-    }
-  })
+      ids,
+    },
+  }).then((res) => {
+    const song = res.songs[0];
+
+    return Song({
+      id: song.id,
+      name: song.name,
+      singer: Singer({ name: song.ar.name }),
+      pictureUrl: song.al.picUrl
+    })
+  });
 }
 
 export function getLyric(id) {
   return request({
     url: "/lyric",
     params: {
-      id
-    }
-  })
+      id,
+    },
+  }).then((res) => {
+    return res.lrc.lyric;
+  });
 }
