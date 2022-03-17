61d8de4d9377506a41750732b1b914fcc65e9d0e
sherlinz0 <sherly_cs_sh@163.com>
2022-03-17 14:42:43 +0800

    Fix: 修复常量名称错误

--- src/store/discover/recommend/constants.js
+++ src/store/discover/recommend/constants.js
@@ -1,6 +1,6 @@
 const actionTypes = {
   CHANGE_TOP_BANNERS: "actionTypes/discover/recommend/CHANGE_TOP_BANNERS",
-  CHANGE_HOT_RECOMMEND: "actionTypes/discover/recommend/CHANGE_HOT_RECOMMENDS",
+  CHANGE_HOT_RECOMMENDS: "actionTypes/discover/recommend/CHANGE_HOT_RECOMMENDS",
   CHANGE_NEW_ALBUM: "actionTypes/discover/recommend/CHANGE_NEW_ALBUMS",
 
   CHANGE_UP_RANKING: "actionTypes/discover/recommend/CHANGE_UP_RANKING",
